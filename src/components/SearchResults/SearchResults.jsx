6ce095f7986e02f34393e992552df5d0fb137f76
Elliot Kim <elliotkim916@gmail.com>
2020-11-21 01:48:11 -0800

    Cleaned up search results component

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -3,27 +3,23 @@ import Video from '../Video/Video';
 import styles from './SearchResults.module.css';
 
 const SearchResults = ({ searchedVideo }) => {
-  let videosToRender;
- 
   if (searchedVideo.length) {
-    videosToRender = searchedVideo;
-  } else {
+    return (
+      <div>
+        <h3>Your Search Results</h3>
+
+        <div className={styles.searchResults}>
+          { searchedVideo.map((video, index) => <Video data={video} key={index} />) }
+        </div>
+      </div>
+    );
+} else {
     return (
       <div>
         <h3>No search results, try a new search!</h3>
       </div>
-    )
+    );
   }
-  
-  return (
-    <div>
-      <h3>Your Search Results</h3>
-
-      <div className={styles.searchResults}>
-        { videosToRender.map((video, index) => <Video data={video} key={index} />) }
-      </div>
-    </div>
-  );
-};
+}
 
 export default SearchResults;
